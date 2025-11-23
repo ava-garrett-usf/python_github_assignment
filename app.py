b96369ff48964b927b0a6cc21cb4e3392e9dfd27
ava-garrett-usf <arg25@usf.edu>
2025-11-23 16:06:31 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-#1
+#1 Welcome message
 print("Welcome to my Python program!")
-#2 
+#2 Hours studied input
 hours_studied = input("Amount of hours youd studied today")
-#3
+#3 Weekly goal calculation
 hours_studied = float(hours_studied)
 weekly_studied = hours_studied * 7
+#4 display
+print(f"If you keep this momentum, you will study {weekly_studied} hours this week!")
+#5 Error handling
+try:
+    hours_studied = float(hours_studied)
+except ValueError:
+    print("Please enter a valid integer for hours studied.")
+    exit()
+#
